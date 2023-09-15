914f8a2ff1ab76fa80757d654b9913935b1c4bbb
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-09-15 11:35:48 +0200

    Initial commit

---
+++ src/main/java/com/example/asterixapi/AsterixApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.asterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsterixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsterixApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/asterixapi/AsterixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asterixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsterixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
